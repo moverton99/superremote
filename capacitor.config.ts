026d3cc1a4697954f98799558e06e55183f3cc5e
Mark Overton <37475995+moverton99@users.noreply.github.com>
2025-05-21 12:20:15 -0400

    Initial Vite+React+MUI+Capacitor project setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.superremote.app',
+  appName: 'superremote',
+  webDir: 'dist'
+};
+
+export default config;
